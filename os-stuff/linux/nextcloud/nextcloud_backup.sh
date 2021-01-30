e903b3fbd1fe23a4a45add4b0615a3a7741ad442
majojoe <maier_jo@gmx.de>
2021-01-30 22:43:47 +0100

    :internal: update nextcloud backup script

--- os-stuff/linux/nextcloud/nextcloud_backup.sh
+++ os-stuff/linux/nextcloud/nextcloud_backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Output to a logfile
-BACKUP_DIR="/root/"    
+BACKUP_DIR="/home/ncbackup/"    
+KEEP_ONLY_ONE_BACKUP=1
 
 #create Backup directory if not existing
 if [ ! -d "${BACKUP_DIR}/Backups/" ]; then
@@ -12,24 +13,35 @@ fi
 LOGFILE="${BACKUP_DIR}/Backups/Logs/$(date '+%Y-%m-%d_%H-%M-%S').txt"
 echo "logging to logfile: ${LOGFILE}"
 exec &> >(tee -a "${LOGFILE}")
-echo "Starting Nextcloud export..."
 
-# Run a Nextcloud backup (apps, database, configuration)
-nextcloud.export -a -b -c
-echo "Export complete"
+if [ ${KEEP_ONLY_ONE_BACKUP} -eq 1 ]; then
+        echo "delete all old backups..."
+        rm "${BACKUP_DIR}/Backups/*.tar.gz"
+        echo "old backups deleted"
+fi
 
 echo ""
-echo "remove all old backups, but keep the 2 newest..."
-# Remove all backups but the newest 2 ones
-FILES_TO_REMOVE=$(find "${BACKUP_DIR}/Backups/" -maxdepth 1 -type f -printf '%T@\t"%p"\n' | sort -r -t $'\t' -g | tail +3 | cut -d $'\t' -f 2-)
-#remove old file if any files to remove
-if [ -n "${FILES_TO_REMOVE}" ]; then
-        echo "removing files: "
-        echo "${FILES_TO_REMOVE}"
-        echo "${FILES_TO_REMOVE}" | xargs realpath | xargs -d '\n' rm
-        echo "old backups removed."
-else
-        echo "no old backups to remove."
+echo "Starting Nextcloud export..."
+# Run a Nextcloud backup (-a apps, -b database, -c configuration, -d data)
+#nextcloud.export -a -b -c
+#export all
+nextcloud.export
+echo "Export complete"
+
+echo ""    
+if [ ${KEEP_ONLY_ONE_BACKUP} -eq 0 ]; then
+        echo "remove all old backups, but keep the 2 newest..."
+        # Remove all backups but the newest 2 ones
+        FILES_TO_REMOVE=$(find "${BACKUP_DIR}/Backups/" -maxdepth 1 -type f -printf '%T@\t"%p"\n' | sort -r -t $'\t' -g | tail +3 | cut -d $'\t' -f 2-)
+        #remove old file if any files to remove
+        if [ -n "${FILES_TO_REMOVE}" ]; then
+                echo "removing files: "
+                echo "${FILES_TO_REMOVE}"
+                echo "${FILES_TO_REMOVE}" | xargs realpath | xargs -d '\n' rm
+                echo "old backups removed."
+        else
+                echo "no old backups to remove."
+        fi
 fi
 
 echo ""
@@ -45,8 +57,3 @@ echo "Removing backup Logs older than 50 days..."
 # Remove logs older than 50 days
 find "${BACKUP_DIR}"/Backups/Logs -mtime +50 -type f -delete
 echo "Complete"
-
-echo "Nextcloud backup completed successfully."
-
-
-
--- os-stuff/linux/rsync_mobile_hdd/restore_from_mobile_disk_to_desktop.sh
+++ os-stuff/linux/rsync_mobile_hdd/restore_from_mobile_disk_to_desktop.sh
@@ -2,26 +2,26 @@
 # backup daily all data
 # don't forget the slash at the end of the paths
 echo "Starting syncronization of Download"
-#nice -19 rsync -av --info=progress2 --delete /media/jo/cd57d2cf-1665-4651-a1bc-15496dec5fcd/rsync/Download/ /Download/
+nice -19 rsync -av --info=progress2 --delete /media/jo/bfa82597-dd99-4380-8137-d8f12e8812d5/rsync/Download/ /Download/
 
 echo "Starting syncronization of Dokumente"
-nice -19 rsync -av --info=progress2 --delete /media/jo/cd57d2cf-1665-4651-a1bc-15496dec5fcd/rsync/home_jo/Dokumente/ /home/jo/Dokumente/ 
+nice -19 rsync -av --info=progress2 --delete /media/jo/bfa82597-dd99-4380-8137-d8f12e8812d5/rsync/home_jo/Dokumente/ /home/jo/Dokumente/ 
 
 echo "Starting syncronization of Mail"
-#nice -19 rsync -av --info=progress2 /media/jo/cd57d2cf-1665-4651-a1bc-15496dec5fcd/rsync/home_jo/Mail/ /home/jo/Mail/ 
+#nice -19 rsync -av --info=progress2 /media/jo/bfa82597-dd99-4380-8137-d8f12e8812d5/rsync/home_jo/Mail/ /home/jo/Mail/ 
 
 echo "Starting syncronization of localgit"
-#nice -19 rsync -av --info=progress2 --delete /media/jo/cd57d2cf-1665-4651-a1bc-15496dec5fcd/rsync/home_jo/localgit/ /home/jo/localgit/ 
+nice -19 rsync -av --info=progress2 --delete /media/jo/bfa82597-dd99-4380-8137-d8f12e8812d5/rsync/home_jo/localgit/ /home/jo/localgit/ 
 
 echo "Starting syncronization of Virtualbox VMs"
-#nice -19 rsync -av --info=progress2  --delete "/media/jo/cd57d2cf-1665-4651-a1bc-15496dec5fcd/rsync/home_jo/VirtualBox VMs/" "/home/jo/VirtualBox VMs/"
-#nice -19 rsync -av --info=progress2  --delete "/media/jo/cd57d2cf-1665-4651-a1bc-15496dec5fcd/rsync/home_jo/VirtualBox VMs/Windows 10 64bit/" "/home/jo/VirtualBox VMs/Windows 10 64bit/" 
+#nice -19 rsync -av --info=progress2  --delete "/media/jo/bfa82597-dd99-4380-8137-d8f12e8812d5/rsync/home_jo/VirtualBox VMs/" "/home/jo/VirtualBox VMs/"
+#nice -19 rsync -av --info=progress2  --delete "/media/jo/bfa82597-dd99-4380-8137-d8f12e8812d5/rsync/home_jo/VirtualBox VMs/Windows 10 64bit/" "/home/jo/VirtualBox VMs/Windows 10 64bit/" 
 
 echo "Starting syncronization of public"
-#nice -19 rsync -av --info=progress2 --delete /media/jo/cd57d2cf-1665-4651-a1bc-15496dec5fcd/rsync/public/ /public/ 
+nice -19 rsync -av --info=progress2 --delete /media/jo/bfa82597-dd99-4380-8137-d8f12e8812d5/rsync/public/ /public/ 
 
-sudo nice -19 rsync -av --info=progress2 --delete /media/jo/cd57d2cf-1665-4651-a1bc-15496dec5fcd/rsync/home_ellen/Dokumente/ /home/ellen/Dokumente/ 
-sudo nice -19 rsync -av --info=progress2 --delete /media/jo/cd57d2cf-1665-4651-a1bc-15496dec5fcd/rsync/home_ellen/Bilder/ /home/ellen/Bilder/ 
+sudo nice -19 rsync -av --info=progress2 --delete /media/jo/bfa82597-dd99-4380-8137-d8f12e8812d5/rsync/home_ellen/Dokumente/ /home/ellen/Dokumente/ 
+sudo nice -19 rsync -av --info=progress2 --delete /media/jo/bfa82597-dd99-4380-8137-d8f12e8812d5/rsync/home_ellen/Bilder/ /home/ellen/Bilder/ 
 
 echo "restore complete."
 
